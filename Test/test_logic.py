0e5c06ea14270099fbd3f8cc04f183d9a6bdb998
Le Duy Son <leduyson2632001@gmail.com>
2022-10-23 23:58:18 +0700

    fix

--- Test/test_logic.py
+++ Test/test_logic.py
@@ -3,4 +3,4 @@ import pytest
 from mongodb_crud.utils.SimpleMath import *
 
 def test_sum():
-    assert sum(1, 2) == 4, "Wrong as fuk"
+    assert sum(2, 2) == 4, "Wrong as fuk"
--- Test/unit/test_logic.py
+++ Test/unit/test_logic.py
@@ -1,4 +1,4 @@
 import pytest
 
 def test_sum():
-    assert sum([1, 2]) == 4, "Wrong as fuk"
+    assert sum([2, 2]) == 4, "Wrong as fuk"
