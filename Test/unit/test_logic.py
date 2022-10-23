f4fa470d4e8d86af2ab5e8fd149cbe9be8abca1b
aidenpearce001 <aidenpearcewd01@gmail.com>
2022-10-24 00:18:52 +0700

    Build

--- Test/unit/test_logic.py
+++ Test/unit/test_logic.py
@@ -1,6 +1,7 @@
 import pytest
 
-from .mongodb_crud.utils.SimpleMath import *
+def test_sum_correct(logic) -> None:
+    assert logic.simple_sum(1, 2) == 3, "Failed"
 
-def test_sum():
-    assert sum(1, 2) == 4, "Wrong as fuk"
+def test_sum_fail(logic) -> None:
+    assert logic.simple_sum(1, 2) == 5, "Failed"
