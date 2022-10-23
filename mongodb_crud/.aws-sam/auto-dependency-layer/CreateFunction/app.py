d978c802ec912ccccf32e3a47b7e8f94fc2ae706
aidenpearce001 <aidenpearcewd01@gmail.com>
2022-10-23 20:45:15 +0700

    'CRUD-Lambda-Deploy'

--- mongodb_crud/.aws-sam/auto-dependency-layer/CreateFunction/app.py
+++ mongodb_crud/.aws-sam/auto-dependency-layer/CreateFunction/app.py
@@ -1,46 +1,36 @@
-from pymongo import MongoClient
-import boto3 
+import os, traceback, json
+import boto3
 
-client = boto3.client('ssm')
+env = os.environ["ENV"]
+Database = os.environ["Db"]
+client = boto3.client("ssm")
 
 def load_config(ssm_parameter_path):
     """
-    Load configparser from config stored in SSM Parameter Store
+    Get Parameter from  SSM Parameter Store
     :param ssm_parameter_path: Path to app config in SSM Parameter Store
     :return: ConfigParser holding loaded config
     """
-    configuration = configparser.ConfigParser()
+    
     try:
         # Get all parameters for this app
-        param_details = client.get_parameters_by_path(
-            Path=ssm_parameter_path,
-            Recursive=False,
+        param_details = client.get_parameter(
+            Name=ssm_parameter_path,
             WithDecryption=True
         )
 
-        # Loop through the returned parameters and populate the ConfigParser
-        if 'Parameters' in param_details and len(param_details.get('Parameters')) > 0:
-            for param in param_details.get('Parameters'):
-                param_path_array = param.get('Name').split("/")
-                section_position = len(param_path_array) - 1
-                section_name = param_path_array[section_position]
-                config_values = json.loads(param.get('Value'))
-                config_dict = {section_name: config_values}
-                print("Found configuration: " + str(config_dict))
-                configuration.read_dict(config_dict)
-
+        configuration = param_details.get('Parameter', [])
+        
     except:
         print("Encountered an error loading config from SSM.")
         traceback.print_exc()
     finally:
-        return configuration
+        return configuration["Value"]
 
 def lambda_handler(event, context):
 
-    # uri = "mongodb://" + MongoDBusername + ":" + MongoDBpassword + "@" + MongoDBaddress + "/" + MongoDBname
+    URI = "/" + env + "/" + Database
     # MongoConnector = MongoClient(uri)
 
-    # pass
-
-    config = load_config(full_config_path)
-    return "MyApp config is " + str(config)
+    config = load_config(URI)
+    return config
