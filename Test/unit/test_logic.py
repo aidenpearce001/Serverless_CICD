8a5252e00b4e3cecd65d89bef7a66ca22fb251c3
aidenpearce001 <aidenpearcewd01@gmail.com>
2022-10-24 17:07:39 +0700

    Build

--- Test/unit/test_logic.py
+++ Test/unit/test_logic.py
@@ -3,5 +3,8 @@ import pytest
 def test_sum_correct(logic) -> None:
     assert logic.simple_sum(1, 2) == 3, "Failed"
 
-def test_sum_fail(logic) -> None:
-    assert logic.simple_sum(3, 2) == 5, "Failed"
+def test_sum_correct(logic) -> None:
+    assert logic.multiple(2, 2) == 4, "Failed"
+
+def test_sum_correct(logic) -> None:
+    assert logic.square_root(9) == 3, "Failed"
